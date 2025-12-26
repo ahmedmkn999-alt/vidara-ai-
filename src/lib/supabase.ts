febcb9d3b8919ece3e35133d1eff4dafb60b9723
ahmedmkn999-alt <ahmedmkn999@gmail.com>
2025-12-26 02:59:16 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// السيرفر هيقرأ المفاتيح دي من الإعدادات اللي حطيناها في Vercel
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
